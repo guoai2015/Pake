0736b9114015a9f44cae54928e4f6104c44089c6
郭爱 <guoai111111@126.com>
2024-03-09 21:03:57 +0800

    修复bug6

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -60,7 +60,7 @@ pub fn run_app() {
                 }
 
                 #[cfg(not(target_os = "macos"))]
-                event.window().unwrap().hide().unwrap();
+                event.window().hide().unwrap();
                 // event.window().close().unwrap();
 
                 api.prevent_close();
